cb9c9fa02a7145dbb8d3f6c14c9a143200525bd2
emanuele <emanuele.biotti@gmail.com>
2019-07-16 00:08:44 +0200

    Aggiunta tabella libri

--- resources/views/libri.blade.php
+++ resources/views/libri.blade.php
@@ -3,8 +3,25 @@
 @section('content')
   <h1>Tutti i libri</h1>
 
-  @foreach ($all_books as $book)
-  {{ $book->titolo }} di {{ $book->autore}} <br>
-  @endforeach
+
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Libro</th>
+        <th scope="col">Autore</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($all_books as $book)
+      <tr>
+        <td>{{ $book->titolo }}</td>
+        <td>{{ $book->autore}}</td>
+      </tr>
+        @endforeach
+    </tbody>
+  </table>
+
+
 
 @endsection
